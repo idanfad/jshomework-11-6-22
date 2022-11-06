ae1248e4019f621b860b5ada0bd736ca5d2d3e2b
idanfad <114670264+idanfad@users.noreply.github.com>
2022-11-06 17:45:36 +0200

    Add files via upload

--- 06-11-22 homework/script.js
+++ 06-11-22 homework/script.js
@@ -9,3 +9,17 @@ function calc() {
     }
 
 }
+
+/* Advanced using != operator */
+
+function acalc() {
+    let num1 = prompt('אנא הזן מספר ראשון');
+    let num2 = prompt('אנא הזן מספר שני');
+
+    if (num1 % num2 == 1) {
+        alert('יש שארית');
+    } else if (num1 % num2 != 1) {
+        alert('אין שארית');
+    }
+
+}
